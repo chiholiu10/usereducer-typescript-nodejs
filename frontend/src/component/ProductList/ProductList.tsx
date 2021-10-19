93a90b67dfe0bdbf3a38a0523e22a00d5023bb8e
Chiho <chiho@Chihos-MacBook-Pro.local>
2021-10-20 00:16:11 +0200

    product list

--- frontend/src/component/ProductList/ProductList.tsx
+++ frontend/src/component/ProductList/ProductList.tsx
@@ -1,10 +1,33 @@
 import { FC, useContext } from "react";
 import { Context } from "../../context/context";
 
+interface Product {
+    category?: string;
+    description?: string;
+    id?: number;
+    image?: string;
+    price?: number;
+    rating?: {
+        rate?: number, count?: number
+    },
+    title?: string;
+}
+
 export const ProductList: FC = () => {
-    const SearchContext = useContext(Context);
-    console.log(SearchContext)
+    const appState = useContext(Context);
+    const { loaded, data } = appState.state;
     return (
-        <div>{}</div>
+        <div>{data.map((item: any) => {
+            return (
+                <div key={item.id}>
+                    <div>{item.category}</div>
+                    <div>{item.description}</div>
+                    <div>{item.image}</div>
+                    <div>{item.rating.rate}</div>
+                    <div>{item.rating.count}</div>
+                    <div>{item.title}</div>
+                </div>
+            )
+        })}</div>
     )
 }
